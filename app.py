e92cc9f335c069fa6f3082ae8fd0c27b05cbd05b
JinlongJiang <jinlong@git.com>
2026-03-18 20:56:03 +0800

    deploy to render

--- app.py
+++ app.py
@@ -64,4 +64,4 @@ app.register_blueprint(main_bp)
 
 # ✅ Run Application
 if __name__ == "__main__":
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=int(os.environ.get("PORT", 5000)), debug=True)
